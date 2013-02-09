0f9f79813b789b63c8292e361a5618c25ff505ae
ubourdon <bourdon.ugo@gmail.com>
2013-02-09 17:19:53 +0100

    improve: shorter -> better content type

--- src/main/scala/fr/simply/util/ContentType.scala
+++ src/main/scala/fr/simply/util/ContentType.scala
@@ -1,13 +1,10 @@
 package fr.simply.util
 
-trait ContentType
-
-object ContentType {
-    def apply(contentType: String): ContentType = ContentTypeBuilder(contentType)
+case class ContentType(contentType: String) {
+    override def toString = contentType
 }
 
-sealed case class ContentTypeBuilder(contentType: String) extends ContentType { override def toString = contentType }
-case object Text_Plain extends ContentType { override def toString = "text/plain" }
-case object Text_Json extends ContentType { override def toString = "text/json" }
-case object Text_Html extends ContentType { override def toString = "text/html" }
-case object Image_Jpeg extends ContentType { override def toString = "image/jpeg" }
+object Text_Plain extends ContentType("text/plain")
+object Text_Json extends ContentType("text/json")
+object Text_Html extends ContentType("text/html")
+object Image_Jpeg extends ContentType("image/jpeg")
